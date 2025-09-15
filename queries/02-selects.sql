a634910b15f7d2ddf26fc9cea4c31ee6bcc76d2a
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-09-15 16:05:05 -0300

    finaliza tabela e inserts de artistas

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,1 +1,9 @@
-SELECT * FROM artistas
+SELECT * FROM artistas;
+
+SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, genero_musical FROM artistas;
+
+SELECT nome, pais_origem FROM artistas WHERE ativo = TRUE;
+
+SELECT nome, genero_musical FROM artistas WHERE id = 9 OR id = 3;
