ef3b4a806a2882c8b5333e1fe576dba2daa69fb5
Jvsilvagomes <joao.silvassenai@gmail.com>
2025-09-15 15:42:42 -0300

    finaliza insert da tabela artistas

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -7,6 +7,22 @@ CREATE TABLE artistas (
     genero_musical VARCHAR(30),
     ano_lancamento INTEGER,
     pais_origem VARCHAR(50),
-    musica-famosa VARCHAR(50),
+    musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+-- Query para inserção de dados na tabela artistas --
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'Solo', 1, 'Pop', '2008', 'Reino Unido', 'Rolling in the Deep', TRUE),
+('21 Pilots', 'Banda', 2, 'Rock Alternativo', 'Estados Unidos', 'Stressed Out', TRUE),
+('Coldplay', 'Banda', 4, 'Rock', 1996, 'Reino Unido', 'Fix You', TRUE),
+('Beyoncé', 'Solo', 1, 'R&B', 2003, 'Estados Unidos', 'Halo', TRUE),
+('Daft Punk', 'Dupla', 2, 'Eletrônica', 1993, 'França', 'Get Lucky', FALSE),
+('Arctic Monkeys', 'Banda', 4, 'Indie Rock', 2002, 'Reino Unido', 'Do I Wanna Know?', TRUE),
+('Shakira', 'Solo', 1, 'Pop Latino', 1990, 'Colômbia', 'Hips Don’t Lie', TRUE),
+('Rammstein', 'Banda', 6, 'Industrial Metal', 1994, 'Alemanha', 'Du Hast', TRUE),
+('The Weeknd', 'Solo', 1, 'R&B', 2010, 'Canadá', 'Blinding Lights', TRUE),
+('BTS', 'Grupo', 7, 'K-Pop', 2013, 'Coreia do Sul', 'Dynamite', TRUE),
+('Nirvana', 'Banda', 3, 'Grunge', 1987, 'Estados Unidos', 'Smells Like Teen Spirit', FALSE);
